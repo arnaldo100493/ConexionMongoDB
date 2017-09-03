59a445b23432091be5f17a1b321e966656dbe45a
FABAME <FABAME@192.168.1.11>
2017-09-03 11:54:43 -0500

    El proyecto se ha generado correctamente.

---
+++ ConexionMongoDB/src/modelo/Persona.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author E301
+ */
+public class Persona {
+    
+   private String nombre;
+   private int cedula;
+   private int edad;
+   private int peso;
+   private String sexo;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, int cedula, int edad, int peso, String sexo) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.edad = edad;
+        this.peso = peso;
+        this.sexo = sexo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+   
+   
+}
